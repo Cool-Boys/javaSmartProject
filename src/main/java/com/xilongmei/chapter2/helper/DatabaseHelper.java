b91906022f18675ed50aa412417ae068cdb03da7
yxl-GS <329991137@qq.com>
2017-11-15 17:46:44 +0800

    数据库操作类

---
+++ src/main/java/com/xilongmei/chapter2/helper/DatabaseHelper.java
@@ -0,0 +1,62 @@
+package com.xilongmei.chapter2.helper;
+
+import com.xilongmei.chapter2.service.CustomerService;
+import com.xilongmei.chapter2.util.PropsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public final class DatabaseHelper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+
+    static {
+        Properties conf = PropsUtil.LoadProps("config.properties");
+        DRIVER = conf.getProperty("jdbc.driver");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWORD = conf.getProperty("jdbc.password");
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("config 文件未找到", e);
+
+        }
+
+
+    }
+
+    /***
+     * 获取数据库连接
+     * */
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            LOGGER.error(" 获取数据库连接失败！", e);
+        }
+        return conn;
+    }
+
+    public  static void closeConnection(Connection conn){
+        if (conn!=null){
+            try {
+                conn.close();
+            }
+            catch (SQLException e){
+                LOGGER.error("关闭数据库连接失败！",e);
+            }
+
+        }
+
+    }
+}
--- src/main/java/com/xilongmei/chapter2/service/CustomerService.java
+++ src/main/java/com/xilongmei/chapter2/service/CustomerService.java
@@ -1,11 +1,20 @@
 package com.xilongmei.chapter2.service;
 
 import com.xilongmei.chapter2.model.Customer;
+import com.xilongmei.chapter2.util.PropsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class CustomerService {
+
+
 
     public List<Customer> getCustomerList(String keyword) {
 
@@ -21,7 +30,7 @@ public class CustomerService {
         return false;
     }
 
-    public boolean updateCustomer(long id,Map<String, Object> fieldMap) {
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
         return false;
     }
 
