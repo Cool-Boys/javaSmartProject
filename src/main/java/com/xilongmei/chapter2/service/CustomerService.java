bcb5e684de76457c65b780c2b2a6f96565751881
yxl-GS <329991137@qq.com>
2017-11-15 14:43:57 +0800

    Signed-off-by: yxl-GS <329991137@qq.com>

---
+++ src/main/java/com/xilongmei/chapter2/service/CustomerService.java
@@ -0,0 +1,31 @@
+package com.xilongmei.chapter2.service;
+
+import com.xilongmei.chapter2.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustomerService {
+
+    public List<Customer> getCustomerList(String keyword) {
+
+        return null;
+    }
+
+    public Customer getCustomer(long id) {
+        return null;
+
+    }
+
+    public boolean createCustomer(Map<String, Object> fieldMap) {
+        return false;
+    }
+
+    public boolean updateCustomer(long id,Map<String, Object> fieldMap) {
+        return false;
+    }
+
+    public boolean deleteCustomer(long id) {
+        return false;
+    }
+}
---
+++ src/main/java/test/HelloServlet.java
@@ -0,0 +1,31 @@
+package test;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,
+                resp);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
