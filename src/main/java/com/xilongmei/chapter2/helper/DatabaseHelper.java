52751c463f1ef83837240c906965606d9172d639
Cool-Boys <329991137@qq.com>
2017-11-17 07:22:47 +0800

    yxl commit DatabaseHelper 优化

--- src/main/java/com/xilongmei/chapter2/helper/DatabaseHelper.java
+++ src/main/java/com/xilongmei/chapter2/helper/DatabaseHelper.java
@@ -1,9 +1,11 @@
 package com.xilongmei.chapter2.helper;
 
 import com.sun.org.apache.bcel.internal.generic.NEW;
+import com.sun.org.apache.xerces.internal.xs.datatypes.ObjectList;
 import com.xilongmei.chapter2.service.CustomerService;
 import com.xilongmei.chapter2.util.CollectionUtil;
 import com.xilongmei.chapter2.util.PropsUtil;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
@@ -15,27 +17,26 @@ import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 public final class DatabaseHelper {
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
-    private static final String DRIVER;
-    private static final String URL;
-    private static final String USERNAME;
-    private static final String PASSWORD;
+
     private static final QueryRunner QUERY_RUNNER = new QueryRunner();
     private static final ThreadLocal<Connection> COLLECTION_HOLDER = new ThreadLocal<Connection>();
+    private static final BasicDataSource DATA_SOURCE;
 
     static {
         Properties conf = PropsUtil.LoadProps("config.properties");
-        DRIVER = conf.getProperty("jdbc.driver");
-        URL = conf.getProperty("jdbc.url");
-        USERNAME = conf.getProperty("jdbc.username");
-        PASSWORD = conf.getProperty("jdbc.password");
-
+        String DRIVER = conf.getProperty("jdbc.driver");
+        String URL = conf.getProperty("jdbc.url");
+        String USERNAME = conf.getProperty("jdbc.username");
+        String PASSWORD = conf.getProperty("jdbc.password");
+        DATA_SOURCE=new BasicDataSource();
+        DATA_SOURCE.setDriverClassName(DRIVER);
+        DATA_SOURCE.setUrl(URL);
+        DATA_SOURCE.setUsername(USERNAME);
+        DATA_SOURCE.setPassword(PASSWORD);
         try {
             Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
@@ -54,7 +55,7 @@ public final class DatabaseHelper {
 
         if (conn == null) {
             try {
-                conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                conn = DATA_SOURCE.getConnection();
             } catch (SQLException e) {
                 LOGGER.error(" 获取数据库连接失败！", e);
             } finally {
@@ -138,17 +139,66 @@ public final class DatabaseHelper {
     }
 
 
-    public  static <T> boolean insertEntity(Class<T> entityClass, Map<String,Object> feildMap){
-        if(CollectionUtil.isEmpty(feildMap)){
-           LOGGER.error("can not insert entity: fieldMap is empty");
-           return false;
+    public static <T> boolean insertEntity(Class<T> entityClass, Map<String, Object> feildMap) {
+        if (CollectionUtil.isEmpty(feildMap)) {
+            LOGGER.error("can not insert entity: fieldMap is empty");
+            return false;
         }
-        String sql="INSERT INTO "+getTableName(entityClass)+"";
-
+        String sql = "INSERT INTO " + getTableName(entityClass) + "";
+        StringBuilder columns = new StringBuilder("(");
+        StringBuilder values = new StringBuilder("(");
+        for (String fieldName : feildMap.keySet()) {
+            columns.append(fieldName).append(", ");
+            values.append("?, ");
+        }
+        columns.replace(columns.lastIndexOf(", "), columns.length(), ")");
+        values.replace(values.lastIndexOf(", "), values.length(), ")");
+        sql += columns + " VALUES " + values;
+        Object[] params = feildMap.values().toArray();
+        return executeUpdate(sql, params) == 1;
 
     }
 
-    private static  String getTableName(Class<?> entityClass) {
+    public static <T> boolean updateEntity(Class<T> entity, long id, Map<String, Object> feildMap) {
+        if (CollectionUtil.isEmpty(feildMap)) {
+            LOGGER.error("can not insert entity: fieldMap is empty");
+            return false;
+        }
+        String sql = "UPDATE " + getTableName(entity) + " SET";
+        StringBuilder columns = new StringBuilder();
+        for (String fieldName : feildMap.keySet()) {
+            columns.append(fieldName).append("=?, ");
+
+        }
+        sql += columns.substring(0, columns.lastIndexOf(", ")) + " WHERE id=?";
+        List<Object> paramList = new ArrayList<Object>();
+        paramList.addAll(feildMap.values());
+        paramList.add(id);
+        Object[] params = paramList.toArray();
+        return executeUpdate(sql, params) == 1;
+    }
+
+
+    private static int executeUpdate(String sql, Object... params) {
+        int rows = 0;
+        Connection conn = getConnection();
+        try {
+            rows = QUERY_RUNNER.update(conn, sql, params);
+        } catch (SQLException e) {
+            LOGGER.error("execute Update failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return rows;
+    }
+
+    public static <T> boolean deleteEntity(Class<T> entityClass,long id){
+        String sql="DELETE FROM "+getTableName(entityClass)+" WHERE id=?";
+        return executeUpdate(sql,id)==1;
+
+    }
+    private static String getTableName(Class<?> entityClass) {
         return entityClass.getSimpleName();
     }
 
